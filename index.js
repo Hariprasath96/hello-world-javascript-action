c1c3c7337e5ddd0f502ba12bfd0fe76416bba605
Hari <Hariprasath.V@gds.ey.com>
2022-11-26 21:00:26 +0530

    get data from all json files-2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
 
   const jsonsInDir = fs
     .readdirSync(absolutePath)
-    .filter((file) => fs.extname(file) === ".json");
+    .filter((file) => path.extname(file) === ".json");
   let arr = [];
 
   jsonsInDir.forEach((file) => {
