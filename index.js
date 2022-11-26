19981c9e223eb37a19147e8df2a8b3677a083192
Hari <Hariprasath.V@gds.ey.com>
2022-11-26 12:02:22 +0530

    added sample json o/p

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const jsonArray = [
+    { name: 'repo 1', repoName: 'repo-1', version: 'v1.0.0' },
+    { name: 'repo 2', repoName: 'repo-1', version: 'v1.0.0' },
+    { name: 'repo 3', repoName: 'repo-3', version: 'v1.0.0' },
+    { name: 'test 1', repoName: 'test-1', version: 'v1.0.0' },
+    { name: 'test 2', repoName: 'test-2', version: 'v1.0.0' }
+  ]
+  core.setOutput("time", jsonArray);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
