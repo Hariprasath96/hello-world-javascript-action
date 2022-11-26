a17a59b33fd86494e42c0d26113b7123a67ab93e
Hari <Hariprasath.V@gds.ey.com>
2022-11-26 20:39:19 +0530

    new js change 4

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs-extra');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs-extra");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello Hariiii prazz...!`);
-  const time = (new Date()).toTimeString();
+  const time = new Date().toTimeString();
   const jsonArray = [
-    { name: 'repo 1', repoName: 'repo-1', version: 'v1.0.0' },
-    { name: 'repo 2', repoName: 'repo-1', version: 'v1.0.0' },
-    { name: 'repo 3', repoName: 'repo-3', version: 'v1.0.0' },
-    { name: 'test 1', repoName: 'test-1', version: 'v1.0.0' },
-    { name: 'test 3', repoName: 'test-2', version: 'v1.0.0' }
-  ]
+    { name: "repo 1", repoName: "repo-1", version: "v1.0.0" },
+    { name: "repo 2", repoName: "repo-1", version: "v1.0.0" },
+    { name: "repo 3", repoName: "repo-3", version: "v1.0.0" },
+    { name: "test 1", repoName: "test-1", version: "v1.0.0" },
+    { name: "test 3", repoName: "test-2", version: "v1.0.0" },
+  ];
 
-  const fileData = fs.readFileSync('./products/repo.json');
+  let relativePath = "./products/repo.json";
+  console.log("🚀 ~ file: index.js ~ line 19 ~ relativePath", relativePath);
+  let absolutePath = resolve(relativePath);
+  console.log("🚀 ~ file: index.js ~ line 21 ~ absolutePath", absolutePath);
+
+  const fileData = fs.readFileSync(absolutePath);
   const json = JSON.parse(fileData.toString());
   console.log(`File read output ${json}`);
 
   core.setOutput("time", jsonArray);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
