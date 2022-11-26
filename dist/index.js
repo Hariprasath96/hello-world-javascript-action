5d43196d594e47d83def8b39f6e908e13990b588
Hari <Hariprasath.V@gds.ey.com>
2022-11-26 20:34:24 +0530

    new js change 2

--- dist/index.js
+++ dist/index.js
@@ -12811,7 +12811,7 @@ const fs = __nccwpck_require__(3059);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello Hariiii!`);
+  console.log(`Hello Hariiii prazz...!`);
   const time = (new Date()).toTimeString();
   const jsonArray = [
     { name: 'repo 1', repoName: 'repo-1', version: 'v1.0.0' },
@@ -12821,7 +12821,7 @@ try {
     { name: 'test 3', repoName: 'test-2', version: 'v1.0.0' }
   ]
 
-  const fileData = fs.readFileSync('/products/repo.json', file);
+  const fileData = fs.readFileSync('./products/repo.json', file);
   const json = JSON.parse(fileData.toString());
   console.log(`File read output ${json}`);
 
